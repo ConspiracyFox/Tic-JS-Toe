90b6c85e94003d496f06cdd95d974f2e289b5834
MentorDerGrosse <90867261+MentorDerGrosse@users.noreply.github.com>
2022-01-30 21:00:00 +0100

    Update Tic-JS-Toe (local playing)

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const boxes = Array.from(document.getElementsByClassName('box'));
-const playerText = document.getElementById('playerText');
+const playerText = document.getElementById('won');
 const spaces = [null, null, null, null, null, null, null, null, null];
 const player1 = "O";
 const player2 = "X";
 var currentPlayer = player1;
-let styleString = '';
+const restartBtn = document.getElementById("restartBtn");
 
 
 const drawBoard = () => {
@@ -23,27 +23,27 @@ const drawBoard = () => {
         styleString += 'border-top: 3px solid var(--green);';
     }
     box.style = styleString;
+    playerText.innerHTML = "Player O's turn";
     box.addEventListener('click', boxClicked);
+
     });
 };
 const boxClicked = (e) =>{
   const id = e.target.id;
-  if(playerHasWon()){
-    playerText.innerText = "${currentPlayer} has won";
-    return;
-  }
-  if(!spaces[id]){
+  if(!spaces[id] && !playerHasWon()){
     spaces[id] = currentPlayer;
     e.target.innerText = currentPlayer;
-    if(currentPlayer == player1){
-      document.getElementById("turn").innerHTML = "X";
-      currentPlayer = player2;
+    if(playerHasWon()){
+      playerText.innerText = currentPlayer + " has won!";
+      return;
     }
-    else{
-      document.getElementById("turn").innerHTML = "O";
+    else if(currentPlayer == player1){
+        playerText.innerHTML = "Player X's turn";
+      currentPlayer = player2;
+    } else{
+        playerText.innerHTML = "Player O's turn";
       currentPlayer = player1;
     }
-
   }
 };
   console.log(spaces);
@@ -52,24 +52,54 @@ const boxClicked = (e) =>{
 const playerHasWon = () => {
   if(spaces[0] == currentPlayer){
     if(spaces[1] == currentPlayer && spaces[2] == currentPlayer){
-      console.log("fehler");
-      document.getElementById("won").innerHTML = "Player " + currentPlayer + " has won!";
+      console.log(currentPlayer, "top won");
+      return true;
+    }
+    if(spaces[3] == currentPlayer && spaces[6] == currentPlayer){
+      console.log(currentPlayer, "left won");
       return true;
     }
     if(spaces[4] == currentPlayer && spaces[8] == currentPlayer){
-      console.log("fehler1");
-      document.getElementById("won").innerHTML = "Player " + currentPlayer + " has won!";
-      console.log(currentPlayer, 'wins.')
+      console.log(currentPlayer, "diagonal won");
       return true;
     }
-    if(spaces[3] == currentPlayer && spaces[6] == currentPlayer)
-      console.log("fehler2");
-      document.getElementById("won").innerHTML = "Player " + currentPlayer + " has won!";
-      console.log(currentPlayer, 'wins.')
-      return true;
-  }
+  } else if(spaces[8] == currentPlayer){
+      if(spaces[2] == currentPlayer && spaces[5] == currentPlayer){
+        console.log(currentPlayer, "right won");
+        return true;
+      }
+      if(spaces[6] == currentPlayer && spaces[7] == currentPlayer){
+        console.log(currentPlayer, "bottom won");
+        return true;
+      }
+    }
+    else if(spaces[4] == currentPlayer){
+      if(spaces[1] == currentPlayer && spaces[7] == currentPlayer){
+        console.log(currentPlayer, "mid vert won");
+        return true;
+      }
+      if(spaces[3] == currentPlayer && spaces[5] == currentPlayer){
+        console.log(currentPlayer, "min hor won");
+        return true;
+      }
+      if(spaces[2] == currentPlayer && spaces[6] == currentPlayer){
+        console.log(currentPlayer, "min hor won");
+        return true;
+      }
+    }
 }
-function restart(){
-  location.reload(true);
-}
+
+restartBtn.addEventListener("click", () => {
+  spaces.forEach((space, index) => {
+    spaces[index] = null;
+  });
+  boxes.forEach((box) => {
+    box.innerText = "";
+  });
+  playerText.innerHTML = "Player O's turn";
+
+  currentPlayer = player1;
+});
+
+
 drawBoard();
